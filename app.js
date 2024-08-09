678a78920dc16931ddb11f2bc0376ca5d5841aef
Faiqfarhan707 <faiq365@gmail.com>
2024-08-09 20:41:30 +0500

    Percentage

--- app.js
+++ app.js
@@ -9,3 +9,11 @@ var obtain =  mathmarks +engmarks+chemarks+phymarks+urdumark
 var persantage = obtain/totalmarks * 100
 
 console.log( "you got" ,persantage + " % " )
+// console.log(obtain)
+
+// if(total > 0 && obtained >= 0) {
+//     var percentage = (obtained / total) * 100;
+//     document.getElementById('result').innerText = "Percentage: " + percentage.toFixed(2) + "%";
+// } else {
+//     document.getElementById('result').innerText = "Please enter valid numbers.";
+// }
